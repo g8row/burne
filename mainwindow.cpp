90637f4785679e5afc012a0e662ec4156e53d278
alex <alex@tequila.k.g>
2023-11-06 13:15:44 +0200

    sort files, fix scrolling, resize buttons

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,6 +8,8 @@
 #include <QPushButton>
 #include <QScrollArea>
 #include <QStackedLayout>
+#include <QScroller>
+#include <QLabel>
 
 namespace fs = std::filesystem;
 
@@ -24,21 +26,21 @@ public:
         contextButtonsPdf->setLayout(new QHBoxLayout());
 
         QPushButton* openButton = new QPushButton("Open");
-        openButton->setFixedHeight(70);
-        openButton->setStyleSheet("font-size: 15px;");
+        openButton->setFixedHeight(60);
+        openButton->setStyleSheet("font-size: 20px;");
 
         contextButtonsPdf->layout()->addWidget(openButton);
         QPushButton* printButton = new QPushButton("Print");
-        printButton->setFixedHeight(70);
-        printButton->setStyleSheet("font-size: 15px;");
+        printButton->setFixedHeight(60);
+        printButton->setStyleSheet("font-size: 20px;");
         contextButtonsPdf->layout()->addWidget(printButton);
 
         contextButtonsImg = new QWidget();
         contextButtonsImg->setLayout(new QHBoxLayout());
 
         QPushButton* flashButton = new QPushButton("Flash");
-        flashButton->setFixedHeight(70);
-        flashButton->setStyleSheet("font-size: 15px;");
+        flashButton->setFixedHeight(60);
+        flashButton->setStyleSheet("font-size: 20px;");
         contextButtonsImg->layout()->addWidget(flashButton);
 
         layout->addWidget(contextButtonsImg);
@@ -66,7 +68,7 @@ bool hasEnding (std::string const &fullString, std::string const &ending) {
 QVBoxLayout* filteredLayout(std::string filter, QVector<QPushButton*> buttons){
     QVBoxLayout* layout = new QVBoxLayout();
     for(QPushButton* button : buttons){
-        if(hasEnding(button->text().toStdString(),filter)){
+        if(hasEnding(((QLabel*)(button->children().at(1)))->text().toStdString(),filter)){
             layout->addWidget(button);
             button->setVisible(true);
         }
@@ -80,6 +82,8 @@ class ButtonList : public QScrollArea {
 public:
     ButtonList(QVector<QPushButton*> buttonsVec): QScrollArea(), buttons(buttonsVec){
         setAlignment(Qt::AlignTop);
+        QScroller::grabGesture(this, QScroller::LeftMouseButtonGesture);
+
         blist = new QWidget();
         QVBoxLayout* layout = new QVBoxLayout();
         for(QPushButton* button : buttons){
@@ -124,6 +128,10 @@ public slots:
     }
 };
 
+bool lessThan(QPushButton* b1, QPushButton* b2){
+    return ((QLabel*)(b1->children().at(1)))->text().compare(((QLabel*)(b2->children().at(1)))->text())<0;
+}
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -133,12 +141,12 @@ MainWindow::MainWindow(QWidget *parent)
     std::string path = getenv("HOME");
     path.append("/Downloads");
     struct stat sb;
-
     QVector<QPushButton*> buttonsVec;
 
     ui->centralwidget->setLayout(new QVBoxLayout());
 
     ButtonStack* bStack =  new ButtonStack();
+
 
     for (const auto& entry : fs::directory_iterator(path)) {
 
@@ -151,18 +159,26 @@ MainWindow::MainWindow(QWidget *parent)
         // Testing whether the path points to a
         // non-directory or not If it does, displays path
         if (stat(path, &sb) == 0 && !(sb.st_mode & S_IFDIR)){
-            QPushButton* button = new QPushButton(outfilename.filename().c_str());
+            QPushButton* button = new QPushButton();
             if(hasEnding(outfilename_str,".pdf")){
                 QObject::connect(button,&QPushButton::clicked,bStack,&ButtonStack::displayPdf);
             }
             if(hasEnding(outfilename_str,".img")){
                 QObject::connect(button,&QPushButton::clicked,bStack,&ButtonStack::displayImg);
             }
-            button->setFixedHeight(100);
-            button->setStyleSheet("font-size: 15px;");
+            auto label = new QLabel(outfilename.filename().c_str());
+            label->setWordWrap(true);
+            label->setStyleSheet("font-size: 20px;");
+            auto layout = new QHBoxLayout(button);
+            layout->addWidget(label,0,Qt::AlignCenter);
+
+            button->setMinimumHeight(95);
+            button->setFixedWidth(740);
             buttonsVec.append(button);
         }
     }
+
+    std::sort(buttonsVec.begin(),buttonsVec.end(),lessThan);
 
     ButtonList* buttons = new ButtonList(buttonsVec);
 
@@ -170,26 +186,26 @@ MainWindow::MainWindow(QWidget *parent)
     filters->setLayout(new QHBoxLayout());
 
     QPushButton* pdfFilter = new QPushButton("Pdf");
-    pdfFilter->setFixedHeight(70);
-    pdfFilter->setStyleSheet("font-size: 15px;");
+    pdfFilter->setFixedHeight(60);
+    pdfFilter->setStyleSheet("font-size: 20px;");
     connect(pdfFilter,&QPushButton::clicked,buttons,&ButtonList::setButtonsPdf);
     filters->layout()->addWidget(pdfFilter);
 
     QPushButton* imgFilter = new QPushButton("Img");
-    imgFilter->setFixedHeight(70);
-    imgFilter->setStyleSheet("font-size: 15px;");
+    imgFilter->setFixedHeight(60);
+    imgFilter->setStyleSheet("font-size: 20px;");
     connect(imgFilter,&QPushButton::clicked,buttons,&ButtonList::setButtonsImg);
     filters->layout()->addWidget(imgFilter);
 
     QPushButton* clearFilter = new QPushButton("Clear");
-    clearFilter->setFixedHeight(70);
-    clearFilter->setStyleSheet("font-size: 15px;");
+    clearFilter->setFixedHeight(60);
+    clearFilter->setStyleSheet("font-size: 20px;");
     connect(clearFilter,&QPushButton::clicked,buttons,&ButtonList::setButtonsClear);
     filters->layout()->addWidget(clearFilter);
 
-
     ui->centralwidget->layout()->addWidget(filters);
     ui->centralwidget->layout()->addWidget(buttons);
+    ui->centralwidget->layout()->addItem(new QSpacerItem(0,0));
     ui->centralwidget->layout()->addWidget(bStack);
 
 }
